b48128bf5bc22cdceacdde09076b2c99ef74571c
James <jspencer30@student.rccd.edu>
2020-06-26 20:02:38 -0700

    HashTable and Package WIP, nearly complete

--- hash_table.py
+++ hash_table.py
@@ -1,3 +1,6 @@
+from package import Package
+
+
 class HashTable:
 
     # A data structure class to be utilized for data organization using key-value pairs
@@ -7,7 +10,26 @@ class HashTable:
         for i in range(capacity):
             self.bucket_list.append([])
 
-    # An insertion function that stores all a package's info
-    def insert(self, package):
+    # An insertion function that calls the package constructor and
+    # inserts it into the hash table all
+    def insert(self, id_num, address, city, state, zip_code, deadline, weight, notes, delivery_status='at_hub'):
+        # Create an instance of the package
+        package = Package(id_num, address, city, state, zip_code, deadline, weight, notes, delivery_status)
         # Select the bucket to store the package
         bucket = hash(package) % len(self.bucket_list)
+        bucket_list = self.bucket_list[bucket]
+
+        # Store package into the list of buckets
+        bucket_list.append(package)
+
+    # Look up function that uses a package's ID and returns the correspond package's info
+    def look_up(self, package_id_num):
+        # Identify the bucket that the package is in by its ID
+        bucket = hash(package_id_num) % len(self.bucket_list)
+        # Return the package if it is found in the bucket list
+        if package_id_num in self.bucket_list:
+            package_index = self.bucket_list.index(package_id_num)
+            return self.bucket_list[package_index]
+        # Return nothing if the package is not found
+        else:
+            return None
--- package.py
+++ package.py
@@ -1,11 +1,18 @@
+from enum import Enum
+
+
 class Package:
 
     # A class to define a package that will be delivered by a truck
-    def __init__(self, id_num, address, deadline, city, zip_code, weight, status):
+    # Defines the ID, delivery address, special delivery notes,
+    # and whether it has been delivered, at the hub, or is in transit
+    def __init__(self, id_num, address, city, state, zip_code, deadline, weight, notes, delivery_status):
         self.id_num = id_num
         self.address = address
-        self.deadline = deadline
         self.city = city
+        self.state = state
         self.zip_code = zip_code
+        self.deadline = deadline
         self.weight = weight
-        self.status = status
+        self.notes = notes
+        self.delivery_status = delivery_status
